97460c8f3a8232637efce14e89b4483620dfc530
htrevino29 <htrevino29@gmail.com>
2017-04-14 13:04:59 -0500

    edit

--- public/robarHome.php
+++ public/robarHome.php
@@ -1,3 +1,143 @@
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "htrevino29@gmail.com";
+ 
+    $email_subject = "User Inquiry";
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['last_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['telephone']) ||
+ 
+        !isset($_POST['comments'])
+        ) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name']; // required
+ 
+    $last_name = $_POST['last_name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $telephone = $_POST['telephone']; // not required
+ 
+    $comments = $_POST['comments']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+ 
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($comments) < 2) {
+ 
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+ 
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
 <html lang="en">
 <head>
   <title>Robar Custom Homes</title>
@@ -21,26 +161,26 @@
   <script src="magnific-popup/jquery.magnific-popup.js"></script>
 
   <script type="text/javascript">
-        $('#reg-form').submit(function(e){
+        // $('#reg-form').submit(function(e){
   
-            e.preventDefault(); // Prevent Default Submission
+        //     e.preventDefault(); // Prevent Default Submission
           
-            $.ajax({
-         url: 'send_form_email.php',
-         type: 'POST',
-         data: $(this).serialize(), // it will serialize the form data
-                dataType: 'html'
-            })
-            .done(function(data){
-             $('#form-content2').fadeOut('slow', function(){
-                  $('#form-content2').fadeIn('slow').html(data);
-                });
-             console.log(data);
-            })
-            .fail(function(){
-         alert('Submit Failed ...'); 
-            });
-        });
+        //     $.ajax({
+        //  url: 'send_form_email.php',
+        //  type: 'POST',
+        //  data: $(this).serialize(), // it will serialize the form data
+        //         dataType: 'html'
+        //     })
+        //     .done(function(data){
+        //      $('#form-content2').fadeOut('slow', function(){
+        //           $('#form-content2').fadeIn('slow').html(data);
+        //         });
+        //      console.log(data);
+        //     })
+        //     .fail(function(){
+        //  alert('Submit Failed ...'); 
+        //     });
+        // });
   </script>
 
 </head>
@@ -127,26 +267,29 @@
                 </div>
              
                <div class="form-group">
-               <input type="text" class="form-control inputSpace" name="first_name" id="lname" placeholder="Name" required />
+               <input type="text" class="form-control inputSpace" name="first_name" id="fname" placeholder="First Name" required />
                </div>
-               <br>
+
+               <div class="form-group">
+               <input type="text" class="form-control inputSpace" name="last_name" id="lname" placeholder="Last Name" required />
+               </div>             
                
                <div class="form-group">
-               <input type="text" class="form-control inputSpace" name="email" id="lname" placeholder="Your Email" required />
+               <input type="text" class="form-control inputSpace" name="email" id="email" placeholder="Your Email" required />
                </div>
                    
                <div class="form-group">
-               <input type="text" class="form-control inputSpace" name="telephone" id="lname" placeholder="Phone Number" required />
+               <input type="text" class="form-control inputSpace" name="telephone" id="phone" placeholder="Phone Number" required />
                </div>
                  
                <div class="form-group">
-               <textarea type="textarea" class="form-control" name="comments" id="lname" placeholder="Comments"></textarea>
+               <textarea type="textarea" class="form-control" name="comments" id="comment" placeholder="Comments"></textarea>
                </div>
 
                <hr/>
                   
                <div class="form-group">
-               <button class="btn btn-primary formSub">Start</button> 
+               <input type="submit" name="submit" value="Submit">
 
                      
                </div>
